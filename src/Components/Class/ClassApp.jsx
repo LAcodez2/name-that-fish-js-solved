7fd3e8f86a317901f48661d095b4db2a702e4231
Albert Valderas <albertvalderas052101@gmail.com>
2025-12-30 16:45:05 -0600

    fixed class score

--- src/Components/Class/ClassApp.jsx
+++ src/Components/Class/ClassApp.jsx
@@ -7,15 +7,47 @@ export class ClassApp extends Component {
   state = {
     incorrectCount: 0,
     correctCount: 0,
+    fishesLeft: ["trout", "salmon", "tuna", "shark"],
   };
+
+  handleCorrect = () => {
+    const { fishesLeft, correctCount } = this.state;
+    this.state({
+      correctCount: correctCount + 1,
+      fishesLeft: fishesLeft.slice(1),
+    });
+  };
+
+  handleIncorrect = () => {
+    this.state({ incorrectCount: this.state.incorrectCount + 1 });
+  };
+
   render() {
+    const { correctCount, incorrectCount, fishesLeft } = this.state;
+    const gameOver = fishesLeft.length === 0;
     return (
       <>
-        <>
-          <ClassScoreBoard />
-          <ClassGameBoard />
-        </>
-        {false && <ClassFinalScore />}
+        {!gameOver && (
+          <>
+            <ClassScoreBoard
+              correctCount={correctCount}
+              incorrectCount={incorrectCount}
+              answersLeft={fishesLeft}
+            />
+
+            <ClassGameBoard
+              onCorrectGuess={this.handleCorrect}
+              onWrongGuess={this.handleIncorrect}
+            />
+          </>
+        )}
+
+        {gameOver && (
+          <ClassFinalScore
+            correctCount={correctCount}
+            totalCount={correctCount + incorrectCount}
+          />
+        )}
       </>
     );
   }
