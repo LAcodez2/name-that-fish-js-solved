92861a56216d2b96c8a74d56da88afae4315ce98
Albert Valderas <albertvalderas052101@gmail.com>
2025-12-30 16:27:49 -0600

    fixed functional score

--- src/Components/Functional/FunctionalApp.jsx
+++ src/Components/Functional/FunctionalApp.jsx
@@ -1,13 +1,43 @@
 import { FunctionalGameBoard } from "./FunctionalGameBoard";
 import { FunctionalScoreBoard } from "./FunctionalScoreBoard";
 import { FunctionalFinalScore } from "./FunctionalFinalScore";
+import { useState } from "react";
 
 export function FunctionalApp() {
+  const initialFishes = ["trout", "salmon", "tuna", "shark"];
+
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
+  const [fishesLeft, setFishesLeft] = useState(initialFishes);
+
+  const gameOver = fishesLeft.length === 0;
+
   return (
     <>
-      <FunctionalScoreBoard />
-      <FunctionalGameBoard />
-      {false && <FunctionalFinalScore />}
+      {!gameOver && (
+        <>
+          <FunctionalScoreBoard
+            correctCount={correct}
+            incorrectCount={incorrect}
+            answersLeft={fishesLeft}
+          />
+
+          <FunctionalGameBoard
+            onCorrectGuess={() => {
+              setCorrect(correct + 1);
+              setFishesLeft(fishesLeft.slice(1));
+            }}
+            onWrongGuess={() => setIncorrect(incorrect + 1)}
+          />
+        </>
+      )}
+
+      {gameOver && (
+        <FunctionalFinalScore
+          correctCount={correct}
+          totalCount={correct + incorrect}
+        />
+      )}
     </>
   );
 }
